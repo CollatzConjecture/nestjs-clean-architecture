2034f98984ba8548b971d7918968a18a7775e58f
Jerry Lucas <jerrylcas@gmail.com>
2025-07-23 11:22:48 +0700

    feat: add global response

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,13 +11,19 @@ import { ProfileController } from '@api/controllers/profile.controller';
 import { LoggerMiddleware } from '@application/middlewere/logger.middleware';
 import { TerminusOptionsService } from '@infrastructure/health/terminus-options.check';
 import { ApplicationModule } from '@application/application.module';
+import { ResponseService } from '@application/services/response.service';
+import { ApiExceptionFilter } from '@application/filters/api-exception.filter';
+import { ResponseInterceptor } from '@application/interceptors/response.interceptor';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 100,
-    }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     ApiModule,
     ApplicationModule,
     TerminusModule,
@@ -26,7 +32,18 @@ import { ApplicationModule } from '@application/application.module';
     LoggerModule,
   ],
   controllers: [HelloController],
-  providers: [TerminusOptionsService],
+  providers: [
+    TerminusOptionsService,
+    ResponseService,
+    {
+      provide: APP_FILTER,
+      useClass: ApiExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
